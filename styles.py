a0e5113121d556ff84565624bab3f822455b97b5
a23davidbt <bua.teijeiro.david@gmail.com>
2025-02-11 08:11:26 +0100

    build(): generar archivos de distribución

--- styles.py
+++ styles.py
@@ -4,5 +4,5 @@ def load_stylesheet():
     Carga y devuelve el contenido de styles.qss como una cadena
     '''
 
-    with open("styles.qss","r") as style_file:
+    with open("styles.qss", "r") as style_file:
         return style_file.read()
